dd1fdc4f7ba7a3ed4a93d6fa80a214d3cbf3d4b5
Muukii <muukii.app@gmail.com>
2024-07-02 03:00:03 +0900

    Update initializers

--- Sources/swiftui-indexed-for-each/swiftui_indexed_for_each.swift
+++ Sources/swiftui-indexed-for-each/swiftui_indexed_for_each.swift
@@ -1,18 +1,35 @@
 import SwiftUI
 
-public struct IndexedForEach<Element, Content>: View where Content: View {
+public struct IndexedForEach<Element, ID: Hashable, Content: View>: View {
   
-  let data: Array<Element>
+  private let data: [Element]
   
-  let content: (Array<Element>.Index, Element) -> Content
+  private let content: (Array<Element>.Index, Element) -> Content
   
-  public init(data: Array<Element>, @ViewBuilder content: @escaping (Array<Element>.Index, Element) -> Content) {
+  private let id: KeyPath<IndexedArray<Element>.Element, ID>
+  
+  public init(
+    _ data: [Element],
+    @ViewBuilder content: @escaping (Array<Element>.Index, Element) -> Content
+  ) where Element: Identifiable, ID == Element.ID {
+    
     self.data = data
+    self.content = content
+    self.id = \.1.id
+  }
+  
+  public init(
+    _ data: [Element],
+    id: KeyPath<Element, ID>,
+    @ViewBuilder content: @escaping (Array<Element>.Index, Element) -> Content
+  ) where Data: RandomAccessCollection, ID: Hashable {
+    self.data = data
+    self.id = (\IndexedArray<Element>.Element.1).appending(path: id)
     self.content = content
   }
   
   public var body: some View {
-    ForEach(IndexedArray.init(base: data), id: \.0) { e in
+    ForEach(IndexedArray.init(base: data), id: id) { e in
       content(e.0, e.1)
     }
   }
@@ -70,10 +87,26 @@ struct IndexedArray<SourceElement>: RandomAccessCollection {
   
 }
 
+#if DEBUG
+
+struct Item: Identifiable {
+  let id: Int
+}
+
 #Preview {
   VStack {
-    IndexedForEach(data: [1, 2, 3]) { index, element in
+    IndexedForEach([1, 2, 3], id: \.self) { index, element in
       Text("\(index): \(element)")
     }
   }
 }
+
+#Preview { 
+  VStack {
+    IndexedForEach.init([Item(id: 0)]) { index, element in
+      Text("\(index): \(element)")
+    }
+  }
+}
+
+#endif
