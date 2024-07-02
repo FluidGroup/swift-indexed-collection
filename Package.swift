6c02326a0b30a2e8521d39ec6cef830b5b5c0e10
Hiroshi Kimura <muukii.app@gmail.com>
2024-07-02 20:23:55 +0900

    rename (#2)

--- Package.swift
+++ Package.swift
@@ -1,22 +1,30 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-  name: "swiftui-indexed-for-each",
-  platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS(.v6), .tvOS(.v13)],
+  name: "swift-indexed-collection",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v10_15),
+    .watchOS(.v6),
+    .tvOS(.v13),
+    .visionOS(.v1)
+  ],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
-      name: "swiftui-indexed-for-each",
-      targets: ["swiftui-indexed-for-each"]),
+      name: "IndexedCollection",
+      targets: ["IndexedCollection"]
+    )
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
     .target(
-      name: "swiftui-indexed-for-each"),
-    
+      name: "IndexedCollection"
+    )
+
   ]
 )
